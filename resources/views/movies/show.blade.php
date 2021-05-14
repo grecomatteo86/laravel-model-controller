3201c08fa5fdd64299d14b9303ec4671f9d89358
Matteo <grecomatteo86@gmail.com>
2021-05-14 16:11:24 +0200

    html in show.blade e stampa Film Focus

---
+++ resources/views/movies/show.blade.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Film Focus</title>
+</head>
+<body>
+    <h1>{{$movie->name}}</h1>
+    <h2>{{$movie->author}}</h2>
+    <h2>{{$movie->genre}}</h2>
+    <p>{{$movie->description}}</p>
+</body>
+</html>
