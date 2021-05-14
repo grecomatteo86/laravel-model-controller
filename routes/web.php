b5f9b653336e1d9a4765c766886c30e67a98d601
Matteo <grecomatteo86@gmail.com>
2021-05-14 15:32:39 +0200

    nuova inclusione Controller, impostato resource in Route

--- routes/web.php
+++ routes/web.php
@@ -13,7 +13,6 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', 'MovieController@index');
+Route::resource('movies', 'MovieController');
 
-Route::get('/movie/{id}', 'MovieController@show');
 
