82d8e2d7eea5caa106f9db5646a7fa867d44abb9
Matteo <grecomatteo86@gmail.com>
2021-05-14 15:53:19 +0200

    compilazione index

---
+++ resources/views/movies/index.blade.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Tutti i film</title>
+</head>
+<body>
+
+    <h1>Tutti i film</h1>
+
+    <ul>
+        @foreach ($movies as $val)
+            
+            <h1>{{$val->name}}</h1>
+            <h2>{{$val->author}}</h2>
+            <h2>{{$val->genre}}</h2>
+            <p>{{$val->description}}</p>
+                
+        @endforeach
+    </ul>
+
+    
+</body>
+</html>
