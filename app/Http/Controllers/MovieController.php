3753c65480b20e61f95eb6ad9ebc0203d71308ed
Matteo <grecomatteo86@gmail.com>
2021-05-18 20:36:30 +0200

    implementazione funz destroy, stampa messaggio avvenuta cancellazione

--- app/Http/Controllers/MovieController.php
+++ app/Http/Controllers/MovieController.php
@@ -98,8 +98,10 @@ class MovieController extends Controller
      * @param  int  $id
      * @return \Illuminate\Http\Response
      */
-    public function destroy($id)
+    public function destroy(Movie $movie)
     {
-        //
+        $movie->delete();
+
+        return redirect()->route('movies.index')->with('message', 'Il film è stato eliminato');
     }
 }
--- resources/views/movies/index.blade.php
+++ resources/views/movies/index.blade.php
@@ -55,6 +55,12 @@
 
     </table>
 
+    @if (session('message'))
+        <div class="alert alert-success">
+            {{session('message')}}
+        </div>
+    @endif
+
 @endsection
 
     
