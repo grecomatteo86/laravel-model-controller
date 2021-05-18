bf90176ad722c00493e35b6060a34f0d0351c827
Matteo <grecomatteo86@gmail.com>
2021-05-18 16:55:33 +0200

    messi placeholders in show

--- resources/views/movies/show.blade.php
+++ resources/views/movies/show.blade.php
@@ -1,16 +1,12 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Film Focus</title>
-</head>
-<body>
-    <h1>{{$movie->name}}</h1>
+@extends('layouts.base')
+
+@section('pageTitle')
+    {{$movie->name}}
+@endsection
+
+@section('content')
     <h2>{{$movie->author}}</h2>
     <h2>{{$movie->genre}}</h2>
     <p>{{$movie->description}}</p>
     <a href="{{route('movies.index')}}">Tutti i film</a>
-</body>
-</html>
+@endsection
