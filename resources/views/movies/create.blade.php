294267ccf815888f5ea0a2b5f88e78844b79ed34
Matteo <grecomatteo86@gmail.com>
2021-05-18 18:04:49 +0200

    aggiunto Form in create.blade

--- resources/views/movies/create.blade.php
+++ resources/views/movies/create.blade.php
@@ -6,4 +6,34 @@
 
 @section('content')
     
+    <div>
+
+        <form action="">
+
+            <div class="mb-3">
+                <label for="name">Titolo</label>
+                <input type="text" class="form-control" id="name" name="name" placeholder="Titolo">
+            </div>
+
+            <div class="mb-3">
+                <label for="author">Regista</label>
+                <input type="text" class="form-control" id="author" name="author" placeholder="Regista">
+            </div>
+
+            <div class="mb-3">
+                <label for="genre">Genere</label>
+                <input type="text" class="form-control" id="genre" name="genre" placeholder="Genere">
+            </div>
+
+            <div class="mb-3">
+                <label for="description">Trama</label>
+                <textarea class="form-control" name="description" id="description" rows="10" placeholder="Trama"></textarea>
+            </div>
+
+            <button type="submit" class="btn btn-primary">Submit</button>
+
+        </form>
+    
+    </div>
+
 @endsection
