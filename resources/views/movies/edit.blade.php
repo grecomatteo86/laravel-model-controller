1b79bab97c5a065e9d3d312a41e565b6803c9bdf
Matteo <grecomatteo86@gmail.com>
2021-05-18 20:57:48 +0200

    creazione pagina edit.blade e modifica del titolo

---
+++ resources/views/movies/edit.blade.php
@@ -0,0 +1,51 @@
+@extends('layouts.base')
+
+@section('pageTitle')
+    Modifica il Film {{$movie->name}}
+@endsection
+
+@section('content')
+
+    @if ($errors->any())
+        <div class="alert alert-danger">
+            <ul>
+                @foreach ($errors->all() as $error)
+                    <li>{{$error}}</li>
+                @endforeach
+            </ul>
+        </div>
+    @endif
+    
+    <div>
+
+        <form action="{{route('movies.store')}}" method="POST">
+            @method('POST')
+            @csrf
+
+            <div class="mb-3">
+                <label for="name">Titolo</label>
+                <input type="text" class="form-control" id="name" name="name" placeholder="Titolo" required>
+            </div>
+
+            <div class="mb-3">
+                <label for="author">Regista</label>
+                <input type="text" class="form-control" id="author" name="author" placeholder="Regista" required>
+            </div>
+
+            <div class="mb-3">
+                <label for="genre">Genere</label>
+                <input type="text" class="form-control" id="genre" name="genre" placeholder="Genere" required>
+            </div>
+
+            <div class="mb-3">
+                <label for="description">Trama</label>
+                <textarea class="form-control" name="description" id="description" rows="10" placeholder="Trama" required></textarea>
+            </div>
+
+            <button type="submit" class="btn btn-primary">Submit</button>
+
+        </form>
+    
+    </div>
+
+@endsection
