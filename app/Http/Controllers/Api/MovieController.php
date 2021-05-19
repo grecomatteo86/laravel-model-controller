a129efdccfd504a437e5a376b4b4fed7b2580341
Matteo <grecomatteo86@gmail.com>
2021-05-19 15:39:07 +0200

    creazione Api MovieController e index function

---
+++ app/Http/Controllers/Api/MovieController.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use App\Movie;
+use Illuminate\Http\Request;
+
+class MovieController extends Controller
+{
+    public function index()
+    {
+        $movies = Movie::all();
+
+        return response()->json($movies);
+    }
+}
