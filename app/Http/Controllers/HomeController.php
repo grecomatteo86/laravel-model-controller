679bb2e9836d2da388408e9a56e3b6ea3dc36028
Matteo <grecomatteo86@gmail.com>
2021-05-19 16:29:09 +0200

    creazione pagina prova per Api e importazione vue e axios

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -4,11 +4,14 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 
-// route che ha come unico scopo quello di renindirizzare di default l'utente alla home page
+
 class HomeController extends Controller
 {
     public function index()
     {
-        return redirect()->route('movies.index');
+        // renindirizza di default l'utente alla home page
+        // return redirect()->route('movies.index');
+
+        return view('api.prova');
     }
 }
