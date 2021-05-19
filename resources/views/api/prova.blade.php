786ad3f9fb0d757ddbdbfd52f434574b4ff270a6
Matteo <grecomatteo86@gmail.com>
2021-05-19 16:37:10 +0200

    istanziato div app, creato data e mounted

--- resources/views/api/prova.blade.php
+++ resources/views/api/prova.blade.php
@@ -7,11 +7,30 @@
     <title>Document</title>
 </head>
 <body>
+
     <h1>Proviamo le Api</h1>
+
+    <div id="app"></div>
 
     <!-- axios_cdn -->
     <script src="https://cdnjs.cloudflare.com/ajax/libs/axios/0.21.1/axios.min.js" integrity="sha512-bZS47S7sPOxkjU/4Bt0zrhEtWx0y0CRkhEp8IckzK+ltifIIE9EMIMTuT/mEzoIMewUINruDBIR/jJnbguonqQ==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
     <!-- development version, includes helpful console warnings -->
     <script src="https://cdn.jsdelivr.net/npm/vue@2/dist/vue.js"></script>
+
+    <script>
+        var app = new Vue({
+
+            el:'#app',
+
+            data:{
+                films:[]
+            },
+
+            mounted:function(){
+                console.log('prova');
+            }
+        })
+    </script>
+
 </body>
 </html>
