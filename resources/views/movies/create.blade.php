6a2192d8d659b41982330bc2e23961a274e776f6
Matteo <grecomatteo86@gmail.com>
2021-06-04 18:20:08 +0200

    aggiunto metodo old nella create

--- resources/views/movies/create.blade.php
+++ resources/views/movies/create.blade.php
@@ -24,27 +24,27 @@
 
             <div class="mb-3">
                 <label for="cover_image">Immagine Cover</label>
-                <input type="text" class="form-control" id="cover_image" name="cover_image" placeholder="Url">
+                <input type="text" class="form-control" id="cover_image" name="cover_image" placeholder="Url" value='{{old('cover_image')}}'>
             </div>
 
             <div class="mb-3">
                 <label for="name">Titolo</label>
-                <input type="text" class="form-control" id="name" name="name" placeholder="Titolo" required>
+                <input type="text" class="form-control" id="name" name="name" placeholder="Titolo" value='{{old('name')}}' required>
             </div>
 
             <div class="mb-3">
                 <label for="author">Regista</label>
-                <input type="text" class="form-control" id="author" name="author" placeholder="Regista" required>
+                <input type="text" class="form-control" id="author" name="author" placeholder="Regista" value='{{old('author')}}' required>
             </div>
 
             <div class="mb-3">
                 <label for="genre">Genere</label>
-                <input type="text" class="form-control" id="genre" name="genre" placeholder="Genere" required>
+                <input type="text" class="form-control" id="genre" name="genre" placeholder="Genere" value='{{old('genre')}}' required>
             </div>
 
             <div class="mb-3">
                 <label for="description">Trama</label>
-                <textarea class="form-control" name="description" id="description" rows="10" placeholder="Trama" required></textarea>
+                <textarea class="form-control" name="description" id="description" rows="10" placeholder="Trama" required>{{old('description')}}</textarea>
             </div>
 
             <button type="submit" class="btn btn-primary">Submit</button>
